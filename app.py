c076fd7c99599f22b61108402dc3d20b97c1b106
2bleG <2bleg.akagucci@gmail.com>
2023-11-24 15:36:35 +0100

    adding special characters

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ classes = []
 doc_x = []
 doc_y = []
 
-with open('data.json', 'r') as file:
+with open('data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 for intent in data["intents"]:
